8fca0073b89f1904dc6aad51f64d7f860ddf02f1
ErYoung2 <eryoung2@163.com>
2026-08-31 08:55:20 +0800

    update rebuild script

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker rm exchange_check_app && docker rmi exchange_check_app:latest
+docker build -t exchange_check_app:latest .
 docker run -d \
   --name exchange_check_app \
   -p 8080:8080 \
